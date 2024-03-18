3b1cdb082aa5f70f21fb20057f02186719c4b4da
mdinizreis <m4rcosdiniz@gmail.com>
2024-03-18 13:12:13 +0800

    Added Airtable pagination logic (recursive API calls) for autocomplete dropdown

--- src/components/IngredientsAutocomplete.jsx
+++ src/components/IngredientsAutocomplete.jsx
@@ -5,11 +5,55 @@ import styles from "./Upload.module.css";
 const IngredientsAutocomplete = (props) => {
   const [ingredientOptions, setIngredientOptions] = useState([]);
   const [searchValue, setSearchValue] = useState("");
+  const [offset, setOffset] = useState();
 
   /*====================
-  Fetch ingredient options from Airtable
+  FETCH INGREDIENT OPTIONS FROM AIRTABLE
+  Airtable only returns 100 records per call and on each call returns a 'offset' parameters to be used on the next call (so it continues retrieving from where it left)
+  getNextPage() receives the offset value after the first API call is executed and recursevely calls itself until there is no more offset being returned (reached end of records)
+  Check List Records > Pagination for more info: https://airtable.com/appO4hsxlcXJe17xz/api/docs#curl/table:ingredients:list
   ====================*/
+
+  const getNextPage = (offset) => {
+    console.log("Next page data.offset", offset);
+    fetch(import.meta.env.VITE_AIRTABLE + "ingredients" + "?offset=" + offset, {
+      headers: {
+        Authorization: `Bearer ${import.meta.env.VITE_AIRTABLE_TOKEN}`,
+      },
+    })
+      .then((response) => {
+        console.log(response);
+        return response.json();
+      })
+      .then((data) => {
+        console.log(data);
+
+        // Transform the retrieved data into the expected format
+        const transformedOptions = data.records.map((record) => ({
+          value: record.fields.ingredient,
+          label: record.fields.ingredient,
+        }));
+        setIngredientOptions((prevOptions) => [
+          ...prevOptions,
+          ...transformedOptions,
+        ]);
+        if (data.offset) {
+          setOffset(data.offset);
+          getNextPage(data.offset);
+        }
+      })
+      .catch((error) => {
+        console.error("Error fetching ingredient options:", error);
+      });
+  };
+
+  /*====================
+  FETCH INGREDIENT OPTIONS FROM AIRTABLE
+  First API fetch to be executed as page loads. If there are more than 100 records use the offset parameter returned to call getNextPage() function
+  ====================*/
+
   useEffect(() => {
+    //execute this to get the first page (airtable only returns 100 records per API call)
     fetch(import.meta.env.VITE_AIRTABLE + "ingredients", {
       headers: {
         Authorization: `Bearer ${import.meta.env.VITE_AIRTABLE_TOKEN}`,
@@ -26,9 +70,13 @@ const IngredientsAutocomplete = (props) => {
           value: record.fields.ingredient,
           label: record.fields.ingredient,
         }));
-        setIngredientOptions(transformedOptions);
+        setIngredientOptions((prevOptions) => [
+          ...prevOptions,
+          ...transformedOptions,
+        ]);
         if (data.offset) {
-          //insert recursive call to go all the way to the 1000 items
+          setOffset(data.offset);
+          getNextPage(data.offset);
         }
       })
       .catch((error) => {
@@ -37,6 +85,13 @@ const IngredientsAutocomplete = (props) => {
   }, []); // Using Empty dependency array to fetch data only once
 
   return (
+    /*====================
+  INGREDIENTS DROPDOWN LIST & 'FIND MY RECIPE' BUTTON
+  Dropdown will receive its content from Airtable API call
+  on each dropdown selection call (lifting) parent funtion handleSelectIngredient to update the ingredient list accordingly
+  on submit ('find my recipe' button) calls (lifting) parent function
+  ====================*/
+
     <div className="card-body" style={{ height: "7rem", fontFamily: "Roboto" }}>
       <h4 className="card-title">What Ingredients do you have?</h4>
       <h6 className="card-subtitle mb-2 text-muted">
@@ -45,6 +100,7 @@ const IngredientsAutocomplete = (props) => {
       </h6>
       <div className="row g-0">
         <div className="col-md-12">
+          {/* lifting to parent funtion */}
           <form onSubmit={props.handleSubmit}>
             <div className="input-group">
               <Select
@@ -52,7 +108,7 @@ const IngredientsAutocomplete = (props) => {
                 placeholder="Add more ingredients to your list..."
                 options={ingredientOptions}
                 value={searchValue}
-                onChange={props.handleSelectIngredient} //lifting
+                onChange={props.handleSelectIngredient} //lifting to parent funtion
               />
               <div className={styles.card}>
                 <button type="submit" className="btn btn-primary mb-2">
